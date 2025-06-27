51da7e14bf698cf40db9ed71be044b03488a0f4f
HighQ02 <arlanbirzanov@gmail.com>
2025-06-27 06:14:29 +0500

    storage fix

--- src/Backend/storage.py
+++ src/Backend/storage.py
@@ -1,8 +1,25 @@
+import mimetypes
 import aioboto3
 import boto3
+import os
 from config import BUCKET_NAME, ENDPOINT_URL, ACCESS_KEY, SECRET_KEY
 
 session = aioboto3.Session()
+
+def get_content_type(local_path):
+    content_type, _ = mimetypes.guess_type(local_path)
+    if not content_type:
+        # Попробуйте явно задать по расширению
+        ext = os.path.splitext(local_path)[1].lower()
+        if ext in ['.jpg', '.jpeg']:
+            content_type = 'image/jpeg'
+        elif ext == '.png':
+            content_type = 'image/png'
+        elif ext == '.gif':
+            content_type = 'image/gif'
+        else:
+            content_type = 'application/octet-stream'
+    return content_type
 
 def get_presigned_url(object_name, expires_in=60):
     s3 = boto3.client(
@@ -22,10 +39,7 @@ def get_presigned_url(object_name, expires_in=60):
     )
 
 async def upload_file_to_s3(local_path, object_name):
-    import mimetypes
-    content_type, _ = mimetypes.guess_type(local_path)
-    if not content_type:
-        content_type = "application/octet-stream"
+    content_type = get_content_type(local_path)
     async with session.client(
         "s3",
         endpoint_url=ENDPOINT_URL,
